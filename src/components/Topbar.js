b334b2823c0394792f438586397ce7d9e3a7658a
Bruna Topan <brutopanb@gmail.com>
2021-05-12 15:24:10 -0300

    Preparando review

--- src/components/Topbar.js
+++
@@ -1,8 +0,0 @@
-export default function Topbar() {
-    return (
-        <div className="topBar">
-            <p>FoodCamp</p>
-            <p>Sua comida em 6 minutos</p>
-        </div>
-    );
-}
